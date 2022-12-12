5565f0f7366bd6c4f7a152b67182623a542e2651
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2022-12-12 11:09:43 +0530

    map-function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
 
 function App() {
+  
+  // function clickevent () {
+  //   class student {
+  //     constructor (name,id) {
+  //       this.name = name
+  //       this.id = id
+  //     }
+  //     bus () {
+  //       console.log(`${this.name} and ${this.id}`);
+  //     }
+  //   }
+  //   let d = new student("hardik",Math.floor(Math.random()*1000)).bus();
+  // }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +34,13 @@ function App() {
           RUSHABH SUTARIYA
         </a>
       </header>
+      {data.map((i) => {
+        return (<div key={Math.random()}>
+          element id {i.id}
+          element no. {Math.floor(Math.random()*1000)}
+          {console.log(i)}
+        </div>)
+      })}
     </div>
   );
 }
