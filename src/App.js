20eae718f77a2d4be2e6bded64faee25e00ccf1b
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2022-12-26 08:53:20 +0530

    class-staate

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import './App.css';
-
-function App() {
-  const emp = [
-    {
-      name : "hardik1",
-      email : "hardik1@gmail.com"
-    },
-    {
-      name : "mayur2",
-      email : "mayur2@gmail.com"
-    },
-    {
-      name : "milan3",
-      email : "milan3@gmail.com"
-    }
-  ]
-
-  const [student, setStudent] = useState(emp);
-
-  let onClickChange = () => {
-    let data = student.map((i) => ({...i, name : "blank"}))
-    console.log(data);
-    setStudent(data);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        id: "111",
+        title: "post title 111",
+        desc: "This is a test desc 111",
+        student : {
+          name : "krunal",
+          id : 123
+        },
+        age : 20,
+        valid : true
+      }
+  }
+  onClickChange = () => {
+    this.setState({student : {name : "hardik",id : this.state.valid ? 456 : 123}})
+    this.setState({valid : this.state.valid ? false : true})
   }
 
-  return (
-      <div className='App'>
-        hooks
-        {
-          student.map((i) => {
-            return (
-              <p key={Math.random()}>name is {i.name}</p>
-            )
-          })
-        }
-        <button onClick={onClickChange}>Change</button>
-      </div>
-  )
+  render() {
+    return (
+        <div className='App'>
+          name is {this.state.student.name} <br />
+          id is {this.state.student.id} <br />
+          valid is {this.state.valid ? "yes" : "no"} <br />
+          <button onClick={this.onClickChange}>Change</button>
+        </div>
+    )
+  }
 }
 
 export default App;
