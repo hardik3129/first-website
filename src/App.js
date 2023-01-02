3ad1100cec41574f6c115f5d6c973d4c0a2c6a12
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2023-01-02 15:51:35 +0530

    Add Digital Clock by Function Based

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import Alert from 'react-bootstrap/Alert';
-import Dropdown from 'react-bootstrap/Dropdown';
 
 const App = () => {
+
+  const [time, settime] = useState(new Date().toLocaleTimeString())
+
+  useEffect(() => {
+    setInterval(() => {
+      settime(new Date().toLocaleTimeString())
+    }, 1000);
+  },[])
   
   return (
     <div>
-      <Alert variant="primary">
-          This is a alert—check it out!
-      </Alert>
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+      {time}
     </div>
   )
 }
