89b0f693e27136b0c4fcbcad62ae3e0e76bf6be2
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2023-01-02 09:42:22 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react'
-import { Route, Routes, Link } from 'react-router-dom'
-import About from './pages/about.jsx'
-import Home from './pages/home.jsx'
-import Contact from './pages/contact.jsx'
 import './App.css'
+import Alert from 'react-bootstrap/Alert';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const App = () => {
   
   return (
     <div>
-      <div className='d-flex justify-space'>
-        <div>LoGo</div>
-        <div className='d-flex'>
-          <Link to='/'>Home</Link>
-          <Link to='/About'>About</Link>
-          <Link to='/Contact'>Contact</Link>
-        </div>
-      </div>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/About' element={<About/>} />
-        <Route path='/Contact' element={<Contact/>} />
-      </Routes>
+      <Alert variant="primary">
+          This is a alert—check it out!
+      </Alert>
+      <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
     </div>
   )
 }
