ee23a60a371818d3103e4e53aa1e477b1bc41ee3
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2023-01-04 10:31:30 +0530

    number + or -

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './App.css'
 
 function App() {
+
+  const [count, setcount] = useState(0)
+  const changePlush = () => {
+    if (count < 5) {
+      setcount(count + 1)
+    }
+  }
+  const changeMinus = () => {
+    if (count > 0) {
+      setcount(count - 1)
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          THIS CONTENT IS NEW BRANCH
-        </a>
-      </header>
+      <button onClick={changeMinus}>-</button>
+      {count}
+      <button onClick={changePlush}>+</button>
     </div>
   );
 }
