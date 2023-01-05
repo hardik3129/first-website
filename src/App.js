b006e1ac5d072cc2308fd2aa57ebf3931efe801c
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2023-01-05 10:29:01 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from 'react-router-dom'
-import Header from "./components/header"
 import Home from "./pages/home"
 import Departments from "./pages/departments";
 import Head_foot from './layouts/header_footer'
