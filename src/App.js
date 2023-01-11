3ed34e8bd184cc1db94b9f898f721f0adfd46c29
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2023-01-11 10:02:11 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          THIS CONTENT IS NEW BRANCH
-        </a>
-      </header>
+    <div>
+      {
+        <ul>
+          {['a', 'b', 'c'].map(function(item) {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
+      }
     </div>
   );
 }
