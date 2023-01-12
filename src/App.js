ade7b87586a31d78e5d906c2e99d9a038b5b3a3e
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2023-01-12 10:12:43 +0530

    ConditionalRender

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  let [valid, setvalid] = useState(true)
+
+  let OnChnage = () => {
+    setvalid(valid ? false : true)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          THIS CONTENT IS NEW BRANCH
-        </a>
-      </header>
+      {valid ? <h1>Apoinment Panding</h1> : <h1>Apoinment is ready</h1>}
+      {}
+      <button onClick={OnChnage}>Chnage</button>
     </div>
   );
 }
