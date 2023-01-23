45d70e69e651e10a013c807c8d2a448a4be2fa92
hardik3129 <119831353+hardik3129@users.noreply.github.com>
2023-01-23 08:58:14 +0530

    Docter register form

--- src/pages/Docter_register.js
+++ src/pages/Docter_register.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Field, Form, Formik, ErrorMessage } from 'formik'
+import * as yup from 'yup'
+import 'yup-phone'
 
 const Docter_register = () => {
+  const validate = yup.object().shape({
+    empDoc: yup.mixed().required()
+  })
   return (
     <div>
       <h1>Docter Register</h1>
+      <Formik
+        initialValues={{
+          empDoc: ''
+        }}
+        validationSchema={validate}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        <Form>
+          <Field type='file' name='empDoc' />
+          <ErrorMessage name="empDoc" className='text-danger' component="div" />
+          <div className="text-center"><button type="submit" className='appointment-btn'>Submit</button></div>
+        </Form>
+      </Formik>
     </div>
   )
 }
